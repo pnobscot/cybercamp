1a6d2386f0472a3ae825549163fc719c87d5eb24
pnobscot <braydenhowells2003@gmail.com>
2025-07-02 11:09:32 -0600

    we polished from annas suggestions

--- MadLibs/MadLibs_Forkable.py
+++ MadLibs/MadLibs_Forkable.py
@@ -1,24 +1,25 @@
 def displayInstructions():
     # Here is where you will fill in some instructions!
-    print("Hello World")
+    print("Instructions go here!")
 
 
-def playGame():
-    noun1 = input("Please give me a noun: ")
+def playMadLibs():
 
     # Here is where you will ask for more nouns, verbs, and adverbs
     # You could even throw in some sound effects :)
 
+    noun1 = input("Please give me a noun: ")
+
     # Next, use the inputted words in your story!
-    # The lines below are to remind you of the formatted string, please change them!
+    # To do so, use the formatted string, as shown below:
 
     print(f"Here is your noun: {noun1}!")
     print(f"Oh, and here it is again: {noun1}, just in case you missed it.")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # this is a funky line that just means start the program
     displayInstructions()
-    playGame()
+    playMadLibs()
 
     # Here is a link to the python tutorial slideshow again:
     # https://tinyurl.com/python-slideshow
